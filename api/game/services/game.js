6fbad71d9e60e4fcf2513a23cbd0d82e86391203
Luís Eduardo <le.barros20166@hotmail.com>
2020-11-04 08:51:10 -0400

    feature: finalizando processo de popular games no strapi

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -12,110 +12,162 @@ function timeout(ms){
 }
 
 async function getGameInfo(slug){
-  const jsdom = require('jsdom')
-  const {JSDOM} = jsdom;
-  const body = await axios.get(`https://www.gog.com/game/${slug}`)
-  const dom = new JSDOM(body.data);
+  try{
+    const jsdom = require('jsdom')
+    const {JSDOM} = jsdom;
+    const body = await axios.get(`https://www.gog.com/game/${slug}`)
+    const dom = new JSDOM(body.data);
 
-  const ratingElement = dom.window.document.querySelector(".age-restrictions__icon use")
-  const description = dom.window.document.querySelector(".description");
-  return{
-    rating: ratingElement ? ratingElement.getAttribute("xlink:href").replace(/_/g,"").replace(/[^\w-]+/g,"") : "BR0",
-    short_description: description.textContent.trim().slice(0,160),
-    description:description.innerHTML,
+    const ratingElement = dom.window.document.querySelector(".age-restrictions__icon use")
+    const description = dom.window.document.querySelector(".description");
+    return{
+      rating: ratingElement ? ratingElement.getAttribute("xlink:href").replace(/_/g,"").replace(/[^\w-]+/g,"") : "BR0",
+      short_description: description.textContent.trim().slice(0,160),
+      description:description.innerHTML,
+    }
+  }catch(err){
+    console.log(err,"07")
   }
+
 }
 async function getByName(name,entityName){
-  const item = await strapi.services[entityName].find({name})
-  return item.length ? item[0] : null
+  try{
+    const item = await strapi.services[entityName].find({name})
+    return item.length ? item[0] : null
+  }catch(err){
+    console.log(err,"06")
+  }
+
 }
 
 async function create(name,entityName){
-  const item = await getByName(name,entityName)
-  if(!item){
-    return await strapi.services[entityName].create({
-      name,
-      slug:slugify(name, {lower: true}).replace(/[^A-Za-z0-9-_.~]/g, ""),
-    })
+  try{
+    const item = await getByName(name,entityName)
+    if(!item){
+      return await strapi.services[entityName].create({
+        name,
+        slug:slugify(name, {lower: true}).replace(/[^A-Za-z0-9-_.~]/g, ""),
+      })
+    }
+  }catch(err){
+    console.log(err,"05")
   }
+
 }
 
 async function createManyToManyData(products){
-  const developers = {};
-  const publishers = {};
-  const categories = {};
-  const platforms = {};
+  try{
+    const developers = {};
+    const publishers = {};
+    const categories = {};
+    const platforms = {};
 
-  products.forEach(product => {
-    const {developer, publisher, genres, supportedOperationalSystems} = product;
+    products.forEach(product => {
+      const {developer, publisher, genres, supportedOperationalSystems} = product;
 
-    genres && genres.forEach(item => {
-      categories[item] = true
+      genres && genres.forEach(item => {
+        categories[item] = true
+      })
+      supportedOperationalSystems && supportedOperationalSystems.forEach(item => {
+        platforms[item] = true
+      })
+      developers[developer] = true;
+      publishers[publisher] = true;
     })
-    supportedOperationalSystems && supportedOperationalSystems.forEach(item => {
-      platforms[item] = true
-    })
-    developers[developer] = true;
-    publishers[publisher] = true;
-  })
 
-  return Promise.all([
-    ...Object.keys(developers).map((name)=> create(name,"developer")),
-    ...Object.keys(publishers).map((name)=> create(name,"publisher")),
-    ...Object.keys(categories).map((name)=> create(name,"category")),
-    ...Object.keys(platforms).map((name)=> create(name,"platform"))
-  ])
-
+    return Promise.all([
+      ...Object.keys(developers).map((name)=> create(name,"developer")),
+      ...Object.keys(publishers).map((name)=> create(name,"publisher")),
+      ...Object.keys(categories).map((name)=> create(name,"category")),
+      ...Object.keys(platforms).map((name)=> create(name,"platform"))
+    ])
+  }catch(err){
+    console.log(err,"04")
+  }
 }
 
 async function setImage({image,game,field = "cover"}){
-  const url = `https:${image}_bg_crop_1680x655.jpg`
-  const {data} = await axios.get(url,{responseType: "arraybuffer"})
-  console.log(data);
-  return null
+  try{
+    const url = `https:${image}_bg_crop_1680x655.jpg`
+    const {data} = await axios.get(url,{responseType: "arraybuffer"})
+    const buffer = Buffer.from(data,'base64')
+
+    const FormData = require('form-data');
+    const formData = new FormData();
+
+    formData.append('refId',game.id);
+    formData.append('ref','game');
+    formData.append('field',field)
+    formData.append('files',buffer,{filename:`${game.slug}.jpg`});
+
+    console.log(`Uploading ${field} image: ${game.slug}.jpg`)
+
+    await axios({
+      method: 'POST',
+      url:`htpp://${strapi.config.host}:${strapi.config.port}/upload`,
+      data: formData,
+      headers:{"Content-Type":`multipart/form-data; boundary=${formData._boundary}`}
+    })
+
+    await timeout(2000);
+  }catch(err){
+    console.log(err,"03")
+  }
+
 }
 
 async function createGames(products) {
   await Promise.all(
-    products.map(async (product)=>{
-      const item = await getByName(product.title,"game")
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
 
-      if(!item){
-        console.log(`Creating ${product.title}...`)
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
         const game = await strapi.services.game.create({
-          name:product.title,
-          slug:slugify(product.slug, {lower: true}),
-          price:product.price.amount,
-          release: new Date(
-            Number(product.globalReleaseDate)* 1000
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
           ).toISOString(),
-          categories:await Promise.all(
-            product.genres.map((name)=>getByName(name,"category"))
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
           ),
-          platforms:await Promise.all(
-            product.supportedOperatingSystems.map((name)=>getByName(name,"platform"))
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
           ),
-          developers: [await getByName(product.developer,"developer")],
-          publisher: [await getByName(product.publisher,"publisher")],
-          ...(await getGameInfo(product.slug))
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
         });
-        setImage({image:product.image,game});
+        await setImage({ image: product.image, game });
         await Promise.all(
-          product.gallery.slice(0,5).map(url => setImage({image:url,game,field:"gallery"}))
+          product.gallery
+            .slice(0, 5)
+            .map((url) => setImage({ image: url, game, field: "gallery" }))
         );
+
         await timeout(2000);
-        return game
+
+        return game;
       }
-  })
+    })
   );
 }
 
 module.exports = {
   populate: async (params) =>{
-    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
-    const {data : { products }} = await axios.get(gogApiUrl)
-    await createManyToManyData([products[1],products[2]]);
-    await createGames([products[1],products[2]])
+    try{
+      const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
+      const {data : { products }} = await axios.get(gogApiUrl)
+      await createManyToManyData([products[1],products[2]]);
+      await createGames([products[1],products[2]])
+    }catch(err){
+      console.log(err,"01")
+    }
   }
 
 };
